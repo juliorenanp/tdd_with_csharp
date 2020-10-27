78744510608844e74db840642e3251601e511d8f
Julio Pinheiro <juliorenan@live.com>
2020-10-27 20:39:41 -0300

    feat: Added a new project using Xunit.net

---
+++ Teste.LeilaoOnline/Teste.LeilaoOnline.Testes/LeilaoTestes.cs
@@ -0,0 +1,54 @@
+﻿using Alura.LeilaoOnline.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Teste.LeilaoOnline.Testes
+{
+   public class LeilaoTestes
+    {
+        [Fact]
+        public void LeilaoComVariosLances()
+        {
+            // A - Arranje - cenário
+            var leilao = new Leilao("Van Gogh");
+            var pessoa = new Interessada("Zaraki", leilao);
+            var pessoa2 = new Interessada("Ichigo", leilao);
+
+
+            leilao.RecebeLance(pessoa, 800);
+            leilao.RecebeLance(pessoa2, 900);
+            leilao.RecebeLance(pessoa, 1000);
+            leilao.RecebeLance(pessoa2, 900);
+
+            // Act - método sob teste
+            leilao.TerminaPregao();
+
+            //Assert 
+            var valorEsperado = 1200;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+
+        }
+        [Fact]
+        public void LeilaoComApenasUmLance()
+        {
+            var leilao = new Leilao("Van Gogh");
+            var pessoa = new Interessada("Zaraki", leilao);
+
+            leilao.RecebeLance(pessoa, 800);
+
+            // Act - método sob teste
+            leilao.TerminaPregao();
+
+            //Assert 
+            var valorEsperado = 800;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+
+        }
+    }
+}
